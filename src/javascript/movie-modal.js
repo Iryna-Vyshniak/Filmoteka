dc3ffe50034e826159395886bc0311cd9cac5a7d
Iuliia Sokol <avare779@gmail.com>
2022-11-27 13:46:24 +0200

    Film card can be clicked in any place to open modal, first trailer displays if there is no official ones in the provided list

--- src/javascript/movie-modal.js
+++ src/javascript/movie-modal.js
@@ -7,122 +7,126 @@ import BigPicture from 'bigpicture';
 const movieAPI = new ThemoviedbAPI();
 
 export function getItems() {
-    const lightboxedCard = refs.gallery.childNodes;
-    const allProducts = [...lightboxedCard];
-    lightboxedCard.forEach(item => item.addEventListener('click', openLightbox));
-    return allProducts;
+  const lightboxedCard = refs.gallery.childNodes;
+  const allProducts = [...lightboxedCard];
+  lightboxedCard.forEach(item => item.addEventListener('click', openLightbox));
+  return allProducts;
 }
 
 function getSelectedItem(event, array) {
-    const selectedProduct = array.find(
-        item => item.dataset.id === event.currentTarget.dataset.id
-    );
-    return selectedProduct;
+  const selectedProduct = array.find(
+    item => item.dataset.id === event.currentTarget.dataset.id
+  );
+  return selectedProduct;
 }
 
 function openLightbox(event) {
-    event.preventDefault();
-
-    if (event.target.nodeName !== 'LI' && event.target.nodeName !== 'IMG') {
-        return;
-    }
-    if (
-        event.target.nodeName === 'LI' ||
-        event.target.nodeName === 'IMG' ||
-        event.target.nodeName === 'H2'
-    ) {
-        onFilmCardClick(event);
-    }
+  event.preventDefault();
+  let innerElements = [];
+  [...event.currentTarget.children].forEach(el =>
+    innerElements.push([...el.children])
+  );
+  innerElements = innerElements.flatMap(el => el);
+  if (
+    event.target === event.currentTarget ||
+    [...event.currentTarget.children].includes(event.target) ||
+    innerElements.includes(event.target)
+  ) {
+    onFilmCardClick(event);
+  }
+  return;
 }
 
 async function onFilmCardClick(event) {
-    const selectedProduct = await getSelectedItem(event, allProducts);
+  const selectedProduct = await getSelectedItem(event, allProducts);
 
-    const filmId = selectedProduct.dataset.id;
+  const filmId = selectedProduct.dataset.id;
+  try {
+    movieAPI.fetchMovieById(filmId).then(result => {
+      const data = result;
+      const posterPath = data.poster_path
+        ? `https://image.tmdb.org/t/p/w300${data.poster_path}`
+        : `https://astoriamuseums.org/wp-content/uploads/2020/10/OFM-poster-not-available.png`;
 
-    try {
-        movieAPI.fetchMovieById(filmId).then(result => {
-            const data = result;
-            const posterPath = data.poster_path
-                ? `https://image.tmdb.org/t/p/w300${data.poster_path}`
-                : `https://astoriamuseums.org/wp-content/uploads/2020/10/OFM-poster-not-available.png`;
+      const filmData = {
+        id: data.id,
+        poster: posterPath,
+        title: data.title,
+        originalTitle: data.original_title,
+        genres: [],
+        popularity: data.popularity,
+        overview: data.overview,
+        vote_average: data.vote_average,
+        vote_count: data.vote_count,
+      };
 
-            const filmData = {
-                id: data.id,
-                poster: posterPath,
-                title: data.title,
-                originalTitle: data.original_title,
-                genres: [],
-                popularity: data.popularity,
-                overview: data.overview,
-                vote_average: data.vote_average,
-                vote_count: data.vote_count,
-            };
+      data.genres.forEach(genre => {
+        filmData.genres.push(genre.name);
+      });
+      filmData.genres = filmData.genres.join(', ');
 
-            data.genres.forEach(genre => {
-                filmData.genres.push(genre.name);
-            });
-            filmData.genres = filmData.genres.join(', ');
+      createModalMarkUp(filmData);
 
-            createModalMarkUp(filmData);
+      getTrailer(filmId);
 
-            getTrailer(filmId);
+      const addToWatchedBtn = document.querySelector(
+        '.lightbox-modal__watched-button'
+      );
 
-            const addToWatchedBtn = document.querySelector(
-                '.lightbox-modal__watched-button'
-            );
+      const addToQuequeBtn = document.querySelector(
+        '.lightbox-modal__queque-button'
+      );
 
-            const addToQuequeBtn = document.querySelector(
-                '.lightbox-modal__queque-button'
-            );
-
-            addToWatchedBtn.addEventListener('click', onAddToWatchedClick);
-            addToQuequeBtn.addEventListener('click', onAddToQuequeClick);
-        });
-    } catch {
-        er => {
-            console.log(er);
-        };
-    }
+      addToWatchedBtn.addEventListener('click', onAddToWatchedClick);
+      addToQuequeBtn.addEventListener('click', onAddToQuequeClick);
+    });
+  } catch {
+    er => {
+      console.log(er);
+    };
+  }
 }
 
 function getTrailer(filmId) {
-    try {
-        movieAPI.fetchTrailerById(filmId).then(result => {
-            const trailers = result.results;
-            if (trailers.length > 0) {
-                const trailerBtn = document.querySelector('.lightbox-modal__trailer');
-                trailerBtn.classList.remove('is-hidden');
-                const officialTrailer = trailers.find(
-                    el => el.name === 'Official Trailer' || el.name.includes('Official')
-                );
-                const trailerKey = officialTrailer.key;
+  try {
+    movieAPI.fetchTrailerById(filmId).then(result => {
+      const trailers = result.results;
+      if (trailers.length > 0) {
+        const trailerBtn = document.querySelector('.lightbox-modal__trailer');
+        trailerBtn.classList.remove('is-hidden');
+        const officialTrailer = trailers.find(
+          el =>
+            el.name === 'Official Trailer' ||
+            el.name.includes('Official') ||
+            el.name[0]
+        );
+        const trailerKey = officialTrailer.key;
 
-                trailerBtn.addEventListener('click', ontrailerBtnClick);
+        trailerBtn.addEventListener('click', ontrailerBtnClick);
 
-                function ontrailerBtnClick(e) {
-                    BigPicture({
-                        el: e.target,
-                        ytSrc: `${trailerKey}`,
-                    });
-                }
-            }
-        });
-    } catch {
-        er => {
-            console.log(er);
-        };
-    }
+        function ontrailerBtnClick(e) {
+          BigPicture({
+            el: e.target,
+            ytSrc: `${trailerKey}`,
+          });
+        }
+      }
+    });
+  } catch {
+    er => {
+      console.log(er);
+    };
+  }
 }
 
 function onAddToWatchedClick(event) {
-    event.preventDefault();
-    event.target.textContent = 'Added to watched';
-    event.target.disabled = true;
+  event.preventDefault();
+  event.target.textContent = 'Added to watched';
+  event.target.disabled = true;
 }
 
 function onAddToQuequeClick(event) {
-    event.preventDefault();
-    event.target.textContent = 'Added to queque';
-    event.target.disabled = true;
+  event.preventDefault();
+  event.target.textContent = 'Added to queque';
+  event.target.disabled = true;
 }
