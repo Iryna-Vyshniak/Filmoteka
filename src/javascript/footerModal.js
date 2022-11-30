a8e39a86c2a96e323704c3c5337a14e70fa141d3
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-11-30 20:00:11 +0200

    add animation styles to script for modal-team

--- src/javascript/footerModal.js
+++ src/javascript/footerModal.js
@@ -1,28 +1,30 @@
 import { refs } from './refs';
 
 export function callfooterModal() {
-    refs.footerBackDrop.classList.remove('is-hidden');
-    document.querySelector("body").classList.add('noScroll');
-    document.addEventListener('keydown', hideFooterModal);
-    refs.footerBackDrop.addEventListener("click", hideFooterModal);
-    refs.footerCloseBtn.addEventListener("click", footerCloseBtnOnClick)
+  refs.footerBackDrop.classList.remove('is-hidden');
+  document.querySelector('body').classList.add('noScroll');
+  document.querySelector('.team').style.animation =
+    'text 2500ms linear alternate 2500ms';
+  document.addEventListener('keydown', hideFooterModal);
+  refs.footerBackDrop.addEventListener('click', hideFooterModal);
+  refs.footerCloseBtn.addEventListener('click', footerCloseBtnOnClick);
 }
 
 function footerCloseBtnOnClick() {
-    refs.footerBackDrop.classList.add('is-hidden');
-    removeListeners();
+  refs.footerBackDrop.classList.add('is-hidden');
+  removeListeners();
 }
 
 function hideFooterModal(e) {
-    if (e.key === 'Escape' || e.target === refs.footerBackDrop) {
-        refs.footerBackDrop.classList.add('is-hidden');
-        removeListeners();
-    }
+  if (e.key === 'Escape' || e.target === refs.footerBackDrop) {
+    refs.footerBackDrop.classList.add('is-hidden');
+    removeListeners();
+  }
 }
 
 function removeListeners() {
-    document.removeEventListener('keydown', hideFooterModal);
-    refs.footerBackDrop.removeEventListener("click", hideFooterModal);
-    refs.footerCloseBtn.removeEventListener("click", footerCloseBtnOnClick);
-    document.querySelector("body").classList.remove('noScroll');
+  document.removeEventListener('keydown', hideFooterModal);
+  refs.footerBackDrop.removeEventListener('click', hideFooterModal);
+  refs.footerCloseBtn.removeEventListener('click', footerCloseBtnOnClick);
+  document.querySelector('body').classList.remove('noScroll');
 }
